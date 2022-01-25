4c61318eca5dea8bc975bb0b4537dfef0457511f
Elliot Lemaire <lemaireelliot@gmail.com>
2022-01-25 13:04:35 +0100

    Correction useEffect Column.js pour déploiement sur Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Graph from "../components/Graph";
 import s from "../styles/Home.module.scss";
-import data from "../data.json";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className={s.container}>
       <h1>Your progress :</h1>
-      <Graph data={data} />
+      <Graph data={props.data} />
     </div>
   );
+}
+
+import data from "../data.json";
+
+export async function getStaticProps(context) {
+  return {
+    props: { data: data }, // will be passed to the page component as props
+  };
 }
